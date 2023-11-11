0aa3db661cc9ffe85bc5bc6f7239316745a74868
Gabrielhjk <143807216+Gabrielhjk@users.noreply.github.com>
2023-11-10 21:34:59 -0300

    Add files via upload

--- transacoes.py
+++ transacoes.py
@@ -1,3 +1,5 @@
+from cadastro_de_cliente import, cliente
+from cadastro_contas import, conta
 
 class deposito:
     def __init__(self, saldo):
@@ -5,27 +7,28 @@ class deposito:
         print(f"Seu saldo atual é {self.saldo}")
 
     def colocar_saldo(self, dep):
-        self.dep = input("Quanto deseja depositar? ")
+        dep = input("Quanto deseja depositar? ")
 
     def depositado(self, novo_valor):
-        self.novo_valor = self.dep
-        print(f"Seu saldo atual é {self.novo_valor}")
+        novo_valor = self.dep
+        print(f"Seu saldo atual é {novo_valor}")
 
 class saque:
-    def __init__(self, saque):
+    def __init__(self):
         self.saque = input("deseja sacar? ").upper()
         if self.saque in ["S", "SIM"]:
           saque = False
 
-    def retiro_saque(self, retiro_saque):
-        print(f"Saldo suficiente {deposito.depositado(novo_valor)}")
+    def retiro_saque(self):
+        print(f"Saldo suficiente {(deposito.depositado(novo_valor=int))}")
         self.retiro_saque = input("Quanto deseja sacar? ")
         if self.retiro_saque <= 0:
             print("Não é possivel sacar esse valor")
         else:
             print("Retirado com sucesso!")
 
-        return deposito.depositado(novo_valor) - self.retiro_saque
+        b = deposito.depositado(novo_valor=int)
+        return b - self.retiro_saque
 
 class transferencia:
     def __init__(self, transf, pessoa_tran):
@@ -42,8 +45,12 @@ class transferencia:
         else:
             print("Tranferência feita com sucesso!")
 
+class verificar_saldo:
+    def __init__(self):
+        self.verificacao = input("Deseja vereficar saldo da conta? ")
+        if self.verificacao in ['SIM', 'S']:
+            verificar_saldo = False
+
 class historico:
     def __init__(self):
-        return transferencia.saldo_transferencia(saldo_tran)
-
-print(deposito, saque, transferencia)
+        return transferencia.saldo_transferencia(saldo_tran=int)
